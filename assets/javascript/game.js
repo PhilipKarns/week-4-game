2fbf96c4ce15c7008aa069ccd19e472b0c51ee83
Karns <pkarns@deloitte.com>
2017-02-12 18:58:00 -0500

    added variables and started reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,15 @@
 //variables
 var wins = 0;
+console.log(wins);
 var losses = 0;
-var randomNumber;
+console.log(losses);
 var userScore = 0;
+console.log(userScore);
+var randomNumber = Math.floor(Math.random() * 102) + 19;
+$("#randomNumber").html(randomNumber);
+console.log(randomNumber);
+
+//reset function
+function reset() {
+	var randomNumber = Math.floor(Math.random() * 102) + 19;
+}
